5629e5cc6d3e49d6660492cdf2f5005b996addf5
Steven Bower <steven@bower.club>
2026-08-07 12:47:02 +0800

    Map F_VA_F___W.B__QEUK washer to F_V__F___W.B_1QEUK handler
    Provisioned ThinQ2 washer (deviceType 201) reuses the existing front-load
    AABB washer driver until model-specific frame diffs are confirmed.

--- cloud/ha_bridge.ts
+++ cloud/ha_bridge.ts
@@ -52,6 +52,8 @@ const t2deviceTypes: Record<string, T2Factory> = {
     ['F_V__Y___W.B_2QEUK']: F_V8_Y___W_B_2QEUK, // NOTE: we reuse F_V8_Y___W_B_2QEUK as the models appear to be compatible
     ['VCDWL2QEUK']: VCDWL2QEUK, // LG F4X7511TWS front-load washer (matched on modelId VCDWL2QEUK)
     ['F_V__F___W.B_1QEUK']: F_V__F___W_B_1QEUK,
+    // F_VA front-load washer (protocol 4.8 / RTK_RTL8711am); same AABB 0x20 status layout as F_V__F
+    ['F_VA_F___W.B__QEUK']: F_V__F___W_B_1QEUK,
     ['F_VB_F___W.B_2QEUK']: F_VB_F___W_B_2QEUK, // LG CV74J7S2QA washer/dryer combo
     ['T1789EFH_F']: T1789EFH_F, // LG WT7300CW top-loading washer
     ['RV13U6AM8W_D_US_WIFI']: RV13U6AM8W_D_US_WIFI, // LG DLE7300WE dryer
